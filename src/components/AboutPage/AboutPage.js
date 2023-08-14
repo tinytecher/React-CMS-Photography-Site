7ad8e85126a084c046cef3e7066070fd8320c424
tinytecher <tinytecher@gmail.com>
2023-08-14 16:37:14 +0100

    shortened about text and adjusted width

--- src/components/AboutPage/AboutPage.js
+++ src/components/AboutPage/AboutPage.js
@@ -50,26 +50,13 @@ export default function MaternityPage() {
 				<div className='about-text'>
 					<h3 className="about-title">Hey There!</h3>
 					<p>
-						I'm Vivienne, your friendly and affordable
-						photographer. From businesses to professional
-						headshots, families to couples cherishing
-						precious moments, I'm here to capture it all.
-						Whether it's in a cozy studio or the perfect
-						outdoor spot, I specialize in bringing out the
-						emotion in every shot. Photography is an art
-						that thrives on capturing real feelings. My
-						passion lies in turning those emotions into
-						captivating images that truly resonate. Let's
-						make your photography experience enjoyable and
-						memorable – portraiture is where I truly shine.
-						With a background in commercial photography from
-						Dublin and years of experience in London, I've
-						honed my skills to perfection. I've mastered
-						post-production techniques through courses in
-						Photoshop and Lightroom, ensuring your photos
-						are picture-perfect. Feel free to reach out and
-						let's create something incredible together.
-						Vivienne Forte Photographer 📸
+					I'm Vivienne. From businesses to cherished moments, I capture it all. Whether indoors or outdoors, I specialize in evoking emotions through my lens. Photography is my art, portraying genuine feelings in captivating images. Let's create an enjoyable and memorable photography experience together. With a background in commercial photography and honed skills, I ensure your photos are perfect. Reach out and let's make something incredible! Vivienne Forte, Photographer 📸
+
+
+
+
+
+
 					</p>
 				</div>
 			</div>
